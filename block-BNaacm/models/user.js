192e96a25c1bd8f3a446f75c9ede53da6d1e3596
somanshu63 <somanshu63@gmail.com>
2022-08-31 16:40:03 +0530

    learn models in mongoose

---
+++ block-BNaacm/models/user.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.schema;
+
+var userSchema = new Schema({
+    name: {type: String},
+    email: {type: String, lowercase: true},
+    age: {type: Number, default: 0},
+    favourites: [String],
+    marks: [Number],
+    password: {type: String, minlength: 5, maxlength: 15},
+    createdAt: {type: Date, default: Date()}
+});
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;
