8fed976733ca2ae12f5ee7ee7c188c0bb581bcd5
somanshu63 <somanshu63@gmail.com>
2022-08-31 15:16:26 +0530

    learn array, objects, objectid in mongoose schema

---
+++ block-BNaacf/models/user.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.schema;
+
+var userSchema = new Schema({
+    name: {type: String},
+    email: {type: String, lowercase: true},
+    age: {type: Number, default: 0},
+    favourites: [String],
+    marks: [Number]
+});
+
+var addressSchema = new Schema({
+    village: String,
+    city: String,
+    state: String,
+    pin: Number,
+    user: Schema.Type.ObjectId
+});
